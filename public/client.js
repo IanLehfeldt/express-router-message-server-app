394dfdddbefcccf3dbb8411b852166dc7da43091
Luke Schlangen <lukeschlangen@gmail.com>
2017-08-04 15:05:25 -0500

    post route working

--- public/client.js
+++ public/client.js
@@ -2,5 +2,21 @@ console.log('client.js has been loaded');
 
 $(document).ready(function(){
     console.log('jQuery has been loaded');
- 
+    $('#sendMessageButton').on('click', function(){
+        console.log('sendMessageButton was clicked');
+        var nameInput = $('#nameInput').val();
+        var messageInput = $('#messageInput').val();
+        var inputObject = {
+            name: nameInput,
+            message: messageInput
+        };
+        $.ajax({
+            method: 'POST',
+            url: '/message',
+            data: inputObject,
+            success: function(response) {
+                console.log(response);
+            }
+        })
+    });
 });
